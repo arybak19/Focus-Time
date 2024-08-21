4a8845cb71999c77f2b3bbed236bc48c79ef0e4b
Victor <bosilek21@gmail.com>
2024-08-21 16:15:01 -0400

    Updated to where there are no urls or drop down for a sleeker design

--- mainPopup.js
+++ mainPopup.js
@@ -5,13 +5,9 @@ chrome.tabs.query({ currentWindow: true }, function(tabs) {
     const tabList = document.getElementById('tabList');
     tabs.forEach(function(tab) {
         const listItem = document.createElement('li');
-        const details = document.createElement('details');
-        const summary = document.createElement('summary');
-        summary.textContent = tab.title;
-        const tabUrl = document.createTextNode(tab.url);
-        details.appendChild(summary);
-        details.appendChild(tabUrl);
-        listItem.appendChild(details);
+        const tabTitle = document.createElement('b'); // Use <b> for bold
+        tabTitle.textContent = tab.title;
+        listItem.appendChild(tabTitle);
         tabList.appendChild(listItem);
     });
 });

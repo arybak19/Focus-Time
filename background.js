4798b2dbff150e19a911170dc8c03983199acf73
Aiden McLendon <armclend@asu.edu>
2024-09-08 00:48:39 -0700

    Blocked tabs

--- background.js
+++ background.js
@@ -48,17 +48,16 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             }
         }, 1000);
 
-        chrome.webRequest.onBeforeRequest.addListener(
-            function(details) {
-                const url = new URL(details.url);
-                if (url.hostname.endsWith('.gov') || url.hostname.endsWith('.edu') || selectedTabs.includes(details.url)) {
-                    return {cancel: false};
+        chrome.tabs.onCreated.addListener((tab) => {
+            if (timerRunning) {
+                const url = new URL(tab.url);
+                if (!url.hostname.endsWith('.gov') && !url.hostname.endsWith('.edu') && !request.selectedTabs.includes(tab.id)) {
+                    chrome.tabs.remove(tab.id, () => {
+                        console.log('New tab removed:', tab.id);
+                    });
                 }
-                return {cancel: true};
-            },
-            {urls: ["<all_urls>"]},
-            ["blocking"]
-        );
+            }
+        });
 
         // recreate the tabs that were removed, add them to the original window, and end the timer
         setTimeout(() => {

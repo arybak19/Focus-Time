da6edaab74c4cf40a120cdb61b33a5222e1148e9
Aiden McLendon <armclend@asu.edu>
2024-09-16 12:43:09 -0700

    invalid input

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 // Add event listener to the button
-document.getElementById("start").addEventListener("click", submitValue);
+document.getElementById("start").addEventListener("click", function(event) { submitValue(event);});
 
 // Function to set the value in localStorage
-function submitValue() {
+function submitValue(event) {
     var input = document.getElementById('value-input');
-    var value = input.value;
+    var value = parseInt(input.value, 10);
+
+    if (value >= 1 && value <= 120) {
     localStorage.setItem('myValue', value);
+    } else {
+        alert("Please enter a time between 1 and 120 minutes.");
+        event.preventDefault();
+    }
 }
